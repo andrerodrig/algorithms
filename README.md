5e64aa23a8d2289a0644e9401ee8027e10a24449
André Rodrigues <andrelmarques11@gmail.com>
2023-07-30 17:50:09 -0300

    Created Insertion Sort

---
+++ README.md
@@ -0,0 +1,3 @@
+# Algorithms
+
+A library created with aim to study different types of algorithms and analyze their efficiency. Used as bibliography the book _Introduction to Algorithms_, from _Cormen, Thomas H.; Leiserson, Charles E.; Rivest, Ronald L._
---
+++ algorithms/insertion_sort.py
@@ -0,0 +1,15 @@
+from typing import Sequence
+
+
+def insertion_sort(vector: list[int | float]) -> Sequence[int | float]:
+    vector_ = vector.copy()
+
+    for j in range(1, len(vector_)):
+        key = vector_[j]
+        i = j - 1
+        while i > -1 and vector_[i] > key:
+            vector_[i + 1] = vector_[i]
+            i = i - 1
+        vector_[i + 1] = key
+
+    return vector_
