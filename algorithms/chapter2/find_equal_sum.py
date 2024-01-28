2b3e219ad7fdc75a41827ce1c8eae46efc0db930
André Rodrigues <andrelmarques11@gmail.com>
2024-01-28 04:45:29 -0300

    Add chapter 4 and matrix multiplication algorithm

---
+++ algorithms/chapter2/find_equal_sum.py
@@ -0,0 +1,10 @@
+def finds_equal_sum(some_set: set, x: int, length: int) -> None:
+    """Return True if it finds two numbers of the set that the sum
+    result in x. Otherwise returns False.
+
+    :param some_set: The set which the search is passed.
+    :param x: The number equal to the sum of two other numbers if there is it.
+    :param length: The lenght of the set.
+    :return: True if it finds the two numbers, else False.
+    """
+    ...
