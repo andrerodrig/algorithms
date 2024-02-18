d2df28f64a41399dfb53dae5e42e582cda982986
André Rodrigues <andrelmarques11@gmail.com>
2024-02-18 14:08:31 -0300

    Add iterative form of find_max_subarray

--- algorithms/chapter4/find_max_subarray.py
+++ algorithms/chapter4/find_max_subarray.py
@@ -15,7 +15,7 @@ def find_max_crossing_subarray(
     left_sum = -float("inf")
     max_left = -1
     sum = 0
-    for i in range(mid, low + -1, -1):
+    for i in range(mid, low - 1, -1):
         sum = sum + A[i]
         if sum > left_sum:
             left_sum = sum
@@ -34,6 +34,15 @@ def find_max_crossing_subarray(
 
 
 def find_max_subarray(A: list, low: int, high: int) -> tuple[int, int, int | float]:
+    """Recursive form of find maximum subarray algorithm.
+
+    Applies itself to the left side, right side, and the subarray that crosses the
+    middle.
+
+    :param A: The array in which is searched the subarray.
+    :param low: The first index of the array.
+    :param high: The last index of the array.
+    """
     if high == low:
         return (low, high, A[low])
     else:
@@ -48,3 +57,35 @@ def find_max_subarray(A: list, low: int, high: int) -> tuple[int, int, int | flo
             return (right_low, right_high, right_max)
         else:
             return (cross_low, cross_high, cross_max)
+
+
+def find_max_subarray_iterative(A: list) -> tuple[int, int, int | float]:
+    """Iterative form of find maximum subarray.
+
+    Finds the max subarray by brute force, comparing each subarray iteratively.
+
+    :param A: The array used to find the max subarray.
+    :returns: The left and right indexes of the subarray and it's value.
+    """
+    max_value = -float("inf")
+    max_index_left = max_index_right = -1
+    sum_value = 0
+
+    for i in range(len(A)):
+        sum_value = A[i]
+
+        if sum_value >= max_value:
+            max_value = sum_value
+            max_index_left = max_index_right = i
+
+        for j in range(i, len(A)):
+            if i == j:
+                continue
+
+            sum_value += A[j]
+            if sum_value >= max_value:
+                max_value = sum_value
+                max_index_left = i
+                max_index_right = j
+
+    return (max_index_left, max_index_right, max_value)
--- tests/chapter4/test_find_max_subarray.py
+++ tests/chapter4/test_find_max_subarray.py
@@ -2,6 +2,7 @@ import pytest
 from algorithms.chapter4.find_max_subarray import (
     find_max_crossing_subarray,
     find_max_subarray,
+    find_max_subarray_iterative,
 )
 
 
@@ -33,9 +34,32 @@ def test_find_max_crossing_subarray(
             [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7],
             (7, 10, 43),
         ),
+        ([-1, -32, -3, -4, -33, -34, -23, -6, -18, -10, -2, -15, -18, -8], (0, 0, -1)),
+        ([-11, -2, -33, -12, -33, -4], (1, 1, -2)),
     ),
 )
 def test_find_max_subarray(array: list, expected_result: tuple[int, int, int]) -> None:
     left_index, right_index, max_value = find_max_subarray(array, 0, len(array) - 1)
+
+    assert (left_index, right_index, max_value) == expected_result
+
+
+@pytest.mark.parametrize(
+    ["array", "expected_result"],
+    (
+        ([1, 32, -5, 23, -19, 10], (0, 3, 51)),
+        ([1, 32, -3, 4, -33, -34, 23, 6, 18, -10, -2, -15, 18, 8], (6, 8, 47)),
+        (
+            [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7],
+            (7, 10, 43),
+        ),
+        ([-1, -32, -3, -4, -33, -34, -23, -6, -18, -10, -2, -15, -18, -8], (0, 0, -1)),
+        ([-11, -2, -33, -12, -33, -4], (1, 1, -2)),
+    ),
+)
+def test_find_max_subarray_iterative(
+    array: list, expected_result: tuple[int, int, int]
+) -> None:
+    left_index, right_index, max_value = find_max_subarray_iterative(array)
 
     assert (left_index, right_index, max_value) == expected_result
