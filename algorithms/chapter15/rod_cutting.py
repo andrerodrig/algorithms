7b494ac62e07578c310bd2d42eb5abd669471608
André Rodrigues <andrelmarques11@gmail.com>
2024-09-22 02:09:20 -0300

    Add 15.1-3 Rod cutting with costs

--- algorithms/chapter15/rod_cutting.py
+++ algorithms/chapter15/rod_cutting.py
@@ -4,7 +4,7 @@ Dynamic Programming: Rod-cutting problem
 """
 
 
-def cut_rod(prices: list[float], length: int) -> float:
+def cut_rod(prices: list[float | int], length: int) -> float:
     """Top-down recursive implementation of Rod-cutting algorithm.
 
     :param prices:
@@ -17,29 +17,29 @@ def cut_rod(prices: list[float], length: int) -> float:
     if length == 0:
         return 0
 
-    rev = -float("inf")
+    rev = -1.0
     for i in range(length):
         rev = max(rev, prices[i] + cut_rod(prices, length - i - 1))
     return rev
 
 
-def memoized_cut_rod(prices: list[float], length: int) -> float:
+def memoized_cut_rod(prices: list[float | int], length: int) -> float:
     cache: dict[int, float] = {}
     for i in range(length + 1):
-        cache[i] = -float("inf")
+        cache[i] = -1
 
     return _call_memoized_cut_rod(prices, length, cache)
 
 
 def _call_memoized_cut_rod(
-    prices: list[float], length: int, cache: dict[int, float]
+    prices: list[float | int], length: int, cache: dict[int, float]
 ) -> float:
     if cache[length] >= 0:
         return cache[length]
     if length == 0:
-        rev: float = 0.0
+        rev = 0.0
     else:
-        rev = -float("inf")
+        rev = -1
         for i in range(length):
             rev = max(
                 rev, prices[i] + _call_memoized_cut_rod(prices, length - i - 1, cache)
@@ -47,3 +47,74 @@ def _call_memoized_cut_rod(
 
     cache[length] = rev
     return rev
+
+
+def cut_rod_bottom_up(prices: list[float | int], length: int) -> float:
+    # Initialize cache as a mapping length: price
+    cache: dict[int, float | int] = {}
+    cache[0] = 0
+
+    for i in range(length):
+        rev = -1.0
+        for j in range(i + 1):
+            rev = max(rev, prices[j] + cache[i - j])
+        cache[i + 1] = rev
+
+    return cache[length]
+
+
+def cut_rod_bottom_up_extended(
+    prices: list[float | int], length: int
+) -> tuple[dict[int, float | int], dict[int, int]]:
+    revenue_cache: dict[int, float | int] = {}
+    optimal_size_cache: dict[int, int] = {}
+    revenue_cache[0] = 0
+
+    for i in range(length):
+        rev = -1.0
+        for j in range(i + 1):
+            new_rev = prices[j] + revenue_cache[i - j]
+            if rev < new_rev:
+                rev = new_rev
+                optimal_size_cache[i + 1] = j + 1
+
+        revenue_cache[i + 1] = rev
+    return revenue_cache, optimal_size_cache
+
+
+def print_cut_rod_solution(prices: list[float | int], length: int) -> None:
+    _, optimal_size_cache = cut_rod_bottom_up_extended(prices, length)
+    while length > 0:
+        print(f"optimal_size_cache[{length}]={optimal_size_cache[length]}")
+        length = length - optimal_size_cache[length]
+
+
+def get_cut_rod_optimal_values(
+    prices: list[float | int], length: int
+) -> tuple[float, int]:
+    rev_cache, optimal_size_cache = cut_rod_bottom_up_extended(prices, length)
+    return rev_cache[length], optimal_size_cache[length]
+
+
+# 15.1-3) Consider a modification of the rod-cutting problem in which, in addition to a
+# price p_i for each rod, each cut incurs a fixed cost of c. The revenue associated with
+# a solution is now the sum of the prices of the pieces minus the costs of making the
+# cuts. Give a dynamic-programming algorithm to solve this modified problem.
+def cut_rod_with_cost(
+    prices: list[float | int], length: int, cut_cost: float
+) -> tuple[dict[int, float | int], dict[int, int]]:
+    rev_cache = {}
+    rev_cache[0] = 0.0
+    optimal_size_map = {}
+
+    for i in range(length):
+        rev = -float("inf")
+        for j in range(i + 1):
+            new_rev = prices[j] + rev_cache[i - j] - cut_cost
+            if rev < new_rev:
+                rev = new_rev
+                optimal_size_map[i + 1] = j + 1
+
+        rev_cache[i + 1] = rev
+
+    return rev_cache, optimal_size_map
--- tests/chapter15/test_rod_cutting.py
+++ tests/chapter15/test_rod_cutting.py
@@ -1,5 +1,12 @@
 import pytest
-from algorithms.chapter15.rod_cutting import cut_rod, memoized_cut_rod
+from algorithms.chapter15.rod_cutting import (
+    cut_rod,
+    memoized_cut_rod,
+    cut_rod_bottom_up,
+    cut_rod_bottom_up_extended,
+    get_cut_rod_optimal_values,
+    cut_rod_with_cost,
+)
 
 
 @pytest.mark.parametrize(
@@ -34,3 +41,153 @@ def test_memoized_cut_rod(length: int, expected_max_rev: int) -> None:
     max_rev = memoized_cut_rod(prices_table, length)
 
     assert max_rev == expected_max_rev
+
+
+@pytest.mark.parametrize(
+    "length, expected_max_rev",
+    [
+        (4, 10),
+        (5, 13),
+        (6, 17),
+        (10, 30),
+    ],
+)
+def test_cut_rod_bottom_up(length: int, expected_max_rev: int) -> None:
+    prices_table = [1.0, 5.0, 8.0, 9.0, 10.0, 17.0, 17.0, 20.0, 24.0, 30.0]
+
+    max_rev = cut_rod_bottom_up(prices_table, length)
+
+    assert max_rev == expected_max_rev
+
+
+@pytest.mark.parametrize(
+    "length, expected_values",
+    [
+        (4, (10, 2)),
+        (5, (13, 2)),
+        (6, (17, 6)),
+        (10, (30, 10)),
+    ],
+)
+def test_cut_rod_optimal_values(length: int, expected_values: tuple[float, int]) -> None:
+    prices_table = [1.0, 5.0, 8.0, 9.0, 10.0, 17.0, 17.0, 20.0, 24.0, 30.0]
+
+    max_rev = get_cut_rod_optimal_values(prices_table, length)
+
+    assert max_rev == expected_values
+
+
+@pytest.mark.parametrize(
+    "length, expected_values",
+    [
+        (4, ({0: 0, 1: 1.0, 2: 5.0, 3: 8.0, 4: 10.0}, {1: 1, 2: 2, 3: 3, 4: 2})),
+        (
+            5,
+            (
+                {0: 0, 1: 1.0, 2: 5.0, 3: 8.0, 4: 10.0, 5: 13.0},
+                {1: 1, 2: 2, 3: 3, 4: 2, 5: 2},
+            ),
+        ),
+        (
+            6,
+            (
+                {0: 0, 1: 1.0, 2: 5.0, 3: 8.0, 4: 10.0, 5: 13.0, 6: 17.0},
+                {1: 1, 2: 2, 3: 3, 4: 2, 5: 2, 6: 6},
+            ),
+        ),
+        (
+            10,
+            (
+                {
+                    0: 0,
+                    1: 1.0,
+                    2: 5.0,
+                    3: 8.0,
+                    4: 10.0,
+                    5: 13.0,
+                    6: 17.0,
+                    7: 18.0,
+                    8: 22.0,
+                    9: 25.0,
+                    10: 30.0,
+                },
+                {1: 1, 2: 2, 3: 3, 4: 2, 5: 2, 6: 6, 7: 1, 8: 2, 9: 3, 10: 10},
+            ),
+        ),
+    ],
+)
+def test_cut_rod_bottom_up_extended(
+    length: int, expected_values: tuple[float, int]
+) -> None:
+    prices_table = [1.0, 5.0, 8.0, 9.0, 10.0, 17.0, 17.0, 20.0, 24.0, 30.0]
+
+    prices_and_lengths = cut_rod_bottom_up_extended(prices_table, length)
+
+    assert prices_and_lengths == expected_values
+
+
+@pytest.mark.parametrize(
+    "length, expected_values",
+    [
+        (4, ({0: 0.0, 1: -0.2, 2: 3.8, 3: 6.8, 4: 7.8}, {1: 1, 2: 2, 3: 3, 4: 4})),
+        (
+            5,
+            (
+                {0: 0.0, 1: -0.2, 2: 3.8, 3: 6.8, 4: 7.8, 5: 10.6},
+                {1: 1, 2: 2, 3: 3, 4: 4, 5: 2},
+            ),
+        ),
+        (
+            6,
+            (
+                {0: 0.0, 1: -0.2, 2: 3.8, 3: 6.8, 4: 7.8, 5: 10.6, 6: 15.8},
+                {1: 1, 2: 2, 3: 3, 4: 4, 5: 2, 6: 6},
+            ),
+        ),
+        (
+            10,
+            (
+                {
+                    0: 0.0,
+                    1: -0.2,
+                    2: 3.8,
+                    3: 6.8,
+                    4: 7.8,
+                    5: 10.6,
+                    6: 15.8,
+                    7: 15.8,
+                    8: 19.6,
+                    9: 22.8,
+                    10: 28.8,
+                },
+                {1: 1, 2: 2, 3: 3, 4: 4, 5: 2, 6: 6, 7: 7, 8: 2, 9: 9, 10: 10},
+            ),
+        ),
+    ],
+)
+def test_cut_rod_with_cost(
+    length: int, expected_values: tuple[dict[int, float | int], dict[int, int]]
+) -> None:
+    import numpy as np
+
+    prices_table: list[float | int] = [
+        1.0,
+        5.0,
+        8.0,
+        9.0,
+        10.0,
+        17.0,
+        17.0,
+        20.0,
+        24.0,
+        30.0,
+    ]
+    cost = 1.2
+
+    revs, opt_sizes = cut_rod_with_cost(prices_table, length, cost)
+
+    for r, exp_r in zip(revs.values(), expected_values[0].values()):
+        assert np.isclose(r, exp_r, rtol=1e-07)
+
+    for opt, exp_opt in zip(opt_sizes.values(), expected_values[1].values()):
+        assert opt == exp_opt
